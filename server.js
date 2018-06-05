87d3ac4439e8cbd49b1ebd0793864eaa578a71ed
ntdang <ndang83@gmail.com>
2018-06-05 10:24:53 -0400

    Added models

--- server.js
+++ server.js
@@ -1,35 +1,100 @@
-// Using this template, the cheerio documentation,
-// and what you've learned in class so far, scrape a website
-// of your choice, save information from the page in a result array, and log it to the console.
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
 var cheerio = require("cheerio");
-var request = require("request");
 
-// Make a request call to grab the HTML body from the site of your choice
-request("http://www.billboard.com", function(error, response, html) {
+// Require all models
+var db = require("./models");
 
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(html);
-  
-  // An empty array to save the data that we'll scrape
-  var results = [];
+var PORT = 3000;
 
-  // Select each element in the HTML body from which you want information.
-  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-  // but be sure to visit the package's npm page to see how it works
-  $("li.top-stories__title").each(function(i, element) {
-    console.log(element);
-    var link = $(element).children().attr("href");
-    var title = $(element).children().children().text();
+// Initialize Express
+var app = express();
 
-    // Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-      link: link,
-      title: title
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/billboardDB");
+
+// // Make a request call to grab the HTML body from the site of your choice
+// request("http://www.billboard.com", function(error, response, html) {
+
+//   // Load the HTML into cheerio and save it to a variable
+//   // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+//   var $ = cheerio.load(html);
+
+//   // An empty array to save the data that we'll scrape
+//   var results = [];
+
+//   // Select each element in the HTML body from which you want information.
+//   // NOTE: Cheerio selectors function similarly to jQuery's selectors,
+//   // but be sure to visit the package's npm page to see how it works
+//   $("li.top-stories__title").each(function(i, element) {
+//     console.log(element);
+//     var link = $(element).children().attr("href");
+//     var title = $(element).children().children().text();
+
+//     // Save these results in an object that we'll push into the results array we defined earlier
+//     results.push({
+//       link: link,
+//       title: title
+//     });
+//   });
+
+//   // Log the results once you've looped through each of the elements found with cheerio
+//   console.log(results);
+// });
+
+// Routes
+
+// A GET route for scraping the echoJS website
+app.get("/scrape", function (req, res) {
+  // First, we grab the body of the html with request
+  axios.get("http://www.billboard.com/").then(function (response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("li.top-stories__title").each(function (i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
     });
-  });
 
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
+    // If we were able to successfully scrape and save an Article, send a message to the client
+    res.send("Scrape Complete");
+  });
 });
